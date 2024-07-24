a8dd110f7b5d4d6639fe869adf11634181bf66f2
Alex Jr <echtopy@gmail.com>
2024-07-24 16:22:16 +0300

    Project Update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,28 @@
-import '@/assets/styles/globals.css';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import "@/assets/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
-  title: 'Property Pulse | Find The Perfect Rental',
-  description: 'Find your dream rental property',
-  keywords: 'rental, find rentals, find properties',
+  title: "Property Pulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <div>{children}</div>
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+          <ToastContainer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
